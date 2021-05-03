f49bf3514e642a75a7a77a86534d4908eeb4e4f3
Rishi Shah <rshah36@mit.edu>
2021-05-03 13:42:24 -0400

    compiled all voter data into CSVs

--- preprocessing/compileByZip.py
+++ preprocessing/compileByZip.py
@@ -3,15 +3,18 @@
 import pandas as pd
 import json
 import os
+from tqdm import tqdm
 
-for fname in os.listdir('public/data/zipcode_csvs_small'):
+for fname in tqdm(os.listdir('public/data/zipcode_csvs_small')):
     zipCode = fname.split('.')[0]
 
     df = pd.read_csv('public/data/zipcode_csvs_small/' + fname)
+    # print(df.columns)
 
     scoresDf = pd.read_json('public/data/zipcode_voters/' + zipCode + '.json')
+    # print(scoresDf.columns)
 
-    joined = pd.concat(df, scoresDf, axis=1)
+    joined = pd.concat([df, scoresDf], axis=1)
 
     with open('public/data/compiledVoters/' + zipCode + '.csv' , 'w') as file:
         joined.to_csv(file)

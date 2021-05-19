e0242bbf36a6f428983519d23982f43485270eb9
Darian Bhathena <darianb@mit.edu>
2021-05-19 08:29:36 -0400

    Show zipcodes on zoom for US house districts

--- src/components/USHouseDistricts.js
+++ src/components/USHouseDistricts.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {firebaseDatabase, getUSHouseDistricts} from "../utils/data";
+import {firebaseDatabase, getOhioZipcodes, getUSHouseDistricts} from "../utils/data";
 import {ComposableMap, Geographies, Geography} from "react-simple-maps";
 import {geoEquirectangular} from "d3-geo";
 import ReactTooltip from "react-tooltip";
@@ -7,16 +7,22 @@ import ReactTooltip from "react-tooltip";
 export default class USHouseDistricts extends Component {
     state = {
         usHouseDistricts: null,
+        ohioZipcodes: null,
         projection: null,
         clickedDistrict: null,
+        clickedZipcode: null,
         tooltipContent: ""
     }
 
     componentDidMount() {
-        getUSHouseDistricts().then(usHouseDistricts => this.setState({
-            usHouseDistricts,
-            projection: geoEquirectangular().fitExtent([[20, 20], [480, 480]], usHouseDistricts)
-        }));
+        const promises = [getUSHouseDistricts(), getOhioZipcodes()];
+        Promise.all(promises).then(([usHouseDistricts, ohioZipcodes]) => {
+            this.setState({
+                usHouseDistricts,
+                ohioZipcodes,
+                projection: geoEquirectangular().fitExtent([[20, 20], [480, 480]], usHouseDistricts)
+            })
+        })
     }
 
     handleGeographyClicked = geography => {
@@ -30,11 +36,26 @@ export default class USHouseDistricts extends Component {
             firebaseDatabase.ref(`summaryStats/congressionalDistricts/${geography.properties.DISTRICT}`)
                 .once('value')
                 .then(snapshot => snapshot.val())
-                .then(data => this.props.setStatData(data, "usHouseDistrict"))
-            this.setState({
-                projection: geoEquirectangular().fitExtent([[20, 20], [480, 480]], geography),
-                clickedDistrict: geography.properties.DISTRICT
-            })
+                .then(data => {
+                    this.setState({
+                        projection: geoEquirectangular().fitExtent([[20, 20], [480, 480]], geography),
+                        clickedDistrict: geography.properties.DISTRICT
+                    })
+                    this.props.setStatData(data, "usHouseDistrict")
+                })
+        }
+    }
+
+    handleZipcodeClicked = geography => {
+        if (geography.properties.ZCTA5CE10 !== this.state.clickedZipcode) {
+            firebaseDatabase.ref(`summaryStats/zipcodes/${geography.properties.ZCTA5CE10}`)
+                .once('value')
+                .then(snapshot => snapshot.val())
+                .then(data => this.props.setStatData(data || {
+                    RESIDENTIAL_ZIP: geography.properties.ZCTA5CE10,
+                    noData: true
+                }, "zipcode"))
+            this.setState({clickedZipcode: geography.properties.ZCTA5CE10})
         }
     }
 
@@ -64,6 +85,27 @@ export default class USHouseDistricts extends Component {
                             />
                         )}
                     </Geographies>
+                    {this.state.clickedDistrict &&
+                    <Geographies geography={this.state.ohioZipcodes}>
+                        {({geographies}) => geographies.map(geography =>
+                            <Geography key={geography.rsmKey}
+                                       geography={geography}
+                                       className="hover-geography"
+                                       stroke="black"
+                                       strokeWidth={this.state.clickedZipcode === geography.properties.ZCTA5CE10 ? 2 : 1}
+                                       fill="transparent"
+                                       fillOpacity={0.6}
+                                       onClick={() => this.handleZipcodeClicked(geography)}
+                                       onMouseEnter={() => this.setState({
+                                           tooltipContent: `<p>Zipcode ${geography.properties.ZCTA5CE10}</p>`,
+                                       })}
+                                       onMouseLeave={() => this.setState({
+                                           tooltipContent: ""
+                                       })}
+                            />
+                        )}
+                    </Geographies>
+                    }
                 </ComposableMap>
                 <ReactTooltip html={true}>{this.state.tooltipContent}</ReactTooltip>
             </>

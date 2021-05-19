766ac14d3665fc95722b69d598562c2d09301b5e
Darian Bhathena <darianb@mit.edu>
2021-05-19 01:48:47 -0400

    Refactor

--- src/components/StatisticsTable.js
+++ src/components/StatisticsTable.js
@@ -10,14 +10,6 @@ export default class StatisticsTable extends Component {
     }
 
     getData = () => {
-        if (this.props.localityType === "usHouseDistrict") {
-            return this.cleanUSHouseDistrictData();
-        } else if (this.props.localityType === 'stateSenateDistrict') {
-            return this.cleanStateSenateDistrictData();
-        }
-    }
-
-    cleanUSHouseDistrictData = () => {
         return {
             Age: `${this.props.statData.avg_age.toFixed(2)} ± ${this.props.statData.age_stddev.toFixed(2)}`,
             "Party Skew": this.props.statData.CODED_PARTY_AFFILIATION.toFixed(5),
@@ -27,18 +19,6 @@ export default class StatisticsTable extends Component {
             "Primary Election AVES": this.props.statData.Primary.toFixed(3),
             "Special Election AVES": this.props.statData.Special.toFixed(3)
         };
-    }
-
-    cleanStateSenateDistrictData = () => {
-        return {
-            Age: `${this.props.statData.avg_age.toFixed(2)} ± ${this.props.statData.age_stddev.toFixed(2)}`,
-            "Party Skew": this.props.statData.CODED_PARTY_AFFILIATION.toFixed(5),
-            "# Registered Voters": this.props.statData["Number of Rows (Aggregated)"],
-            "Overall AVES": this.props.statData.Score.toFixed(3),
-            "General Election AVES": this.props.statData.General.toFixed(3),
-            "Primary Election AVES": this.props.statData.Primary.toFixed(3),
-            "Special Election AVES": this.props.statData.Special.toFixed(3)
-        }
     }
 
     render() {

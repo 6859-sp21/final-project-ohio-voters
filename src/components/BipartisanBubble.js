c64dd5b26a0aa24cea99993dfd66f80f9ba81e09
Darian Bhathena <darianb@mit.edu>
2021-05-19 16:03:39 -0400

    Style scrolly-telling visualizations

--- src/components/BipartisanBubble.js
+++ src/components/BipartisanBubble.js
@@ -1,22 +1,22 @@
 import React, {Component} from "react";
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-import { useState, useEffect } from 'react';
-import { ResponsiveCirclePacking } from '@nivo/circle-packing';
+import {useState, useEffect} from 'react';
+import {ResponsiveCirclePacking} from '@nivo/circle-packing';
 import * as d3 from "d3";
-import {firebaseDatabase,  getBipartisanStats} from '../utils/data'
+import {firebaseDatabase, getBipartisanStats} from '../utils/data'
 
 import "../App.css";
-import { callbackify } from "util";
+import {callbackify} from "util";
 
 const options = [
-  "Ohio House Districts", "Ohio Senate Districts", "US House Districts", "Cities",
+    "Ohio House Districts", "Ohio Senate Districts", "US House Districts", "Cities",
 ];
 const defaultOption = options[0];
 
 export default class BipartisanBubble extends Component {
     state = {
-        stateHouseStats : null,
+        stateHouseStats: null,
         stateSenateStats: null,
         congressionalStats: null,
         cityStats: null,
@@ -26,7 +26,10 @@ export default class BipartisanBubble extends Component {
     }
 
     componentDidMount() {
-        firebaseDatabase.ref(`summaryStats/stateHouseDistricts`).once('value').then(snapshot => snapshot.val()).then(data => this.setState({stateHouseStats: getBipartisanStats(data, "Ohio House District: "), selected: "stateHouseStats"}))
+        firebaseDatabase.ref(`summaryStats/stateHouseDistricts`).once('value').then(snapshot => snapshot.val()).then(data => this.setState({
+            stateHouseStats: getBipartisanStats(data, "Ohio House District: "),
+            selected: "stateHouseStats"
+        }))
         firebaseDatabase.ref(`summaryStats/stateSenateDistricts`).once('value').then(snapshot => snapshot.val()).then(stats => this.setState({stateSenateStats: getBipartisanStats(stats, "Ohio Senate District: ")}));
         firebaseDatabase.ref(`summaryStats/congressionalDistricts`).once('value').then(snapshot => snapshot.val()).then(stats => this.setState({congressionalStats: getBipartisanStats(stats, "US House District: ")}));
         firebaseDatabase.ref(`summaryStats/cities`).once('value').then(snapshot => snapshot.val()).then(stats => this.setState({cityStats: getBipartisanStats(stats, "City: ")}));
@@ -35,88 +38,118 @@ export default class BipartisanBubble extends Component {
 
     render() {
         const changeElements = (val) => {
-          if (val === "Cities") {
-            this.setState({selected : "cityStats"});
-            this.setState({labelSkip : 25});
-          } else if (val === "Ohio House Districts") {
-            this.setState({selected : "stateHouseStats"});
-            this.setState({labelSkip : 10});
-          } else if (val === "Ohio Senate Districts") {
-            this.setState({selected : "stateSenateStats"});
-            this.setState({labelSkip : 10});
-          } else {
-            this.setState({selected : "congressionalStats"});
-            this.setState({labelSkip : 10});
-          }
+            if (val === "Cities") {
+                this.setState({selected: "cityStats"});
+                this.setState({labelSkip: 25});
+            } else if (val === "Ohio House Districts") {
+                this.setState({selected: "stateHouseStats"});
+                this.setState({labelSkip: 10});
+            } else if (val === "Ohio Senate Districts") {
+                this.setState({selected: "stateSenateStats"});
+                this.setState({labelSkip: 10});
+            } else {
+                this.setState({selected: "congressionalStats"});
+                this.setState({labelSkip: 10});
+            }
         };
 
         const getSelected = () => {
-          if (this.state.selected === "cityStats") {
-            return this.state.cityStats;
-          } else if (this.state.selected === "stateHouseStats") {
-            return this.state.stateHouseStats;
-          } else if (this.state.selected === "stateSenateStats") {
-            return this.state.stateSenateStats;
-          } else {
-            return this.state.congressionalStats;
-          }
+            if (this.state.selected === "cityStats") {
+                return this.state.cityStats;
+            } else if (this.state.selected === "stateHouseStats") {
+                return this.state.stateHouseStats;
+            } else if (this.state.selected === "stateSenateStats") {
+                return this.state.stateSenateStats;
+            } else {
+                return this.state.congressionalStats;
+            }
         };
         return (
-          <>
-            <div
-                style={{width:'55%', paddingRight:'5%', paddingBottom:"75px", paddingLeft:"5%", height: "calc(100vh - 150px)", position:"fixed", right:"0", top:"75"}}
-            >
-              <div style={{marginBottom:-85, paddingTop:50, zIndex:100}}> Bipartisan Leans for Ohio AAPIs</div>
-              {this.state.selected && <ResponsiveCirclePacking
-                data={getSelected()}
-                margin={0}
-                id="_id"
-                value="value"
-                colors={(d) => {return d.data.color}}
-                childColor={{ from: 'color', modifiers: [ [ 'brighter', 0.4 ] ] }}
-                colorBy={d => d.color}
-                padding={4}
-                enableLabels={true}
-                labelsFilter={function(e){return 2===e.node.depth}}
-                labelsSkipRadius={this.state.labelSkip}
-                labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 2 ] ] }}
-                borderWidth={1}
-                borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.5 ] ] }}
-                tooltip={(input) => (
-                  <>
-                    {input.data.value &&
-                    <div id="container" style={{backgroundColor: 'rgba(256, 256, 256, 0.4)', padding:"0.25em"}}>
-                      <p style={{lineHeight:0}}>{input.data.type}{input.data._id}</p>
-                      <p style={{lineHeight:0}}>{input.data.skew}</p>
-                      <p style={{lineHeight:0}}>Population: {input.data.population}</p>
+            <>
+                <div
+                    style={{
+                        width: '55%',
+                        paddingRight: '5%',
+                        paddingBottom: "75px",
+                        paddingLeft: "5%",
+                        height: "calc(100vh - 150px)",
+                        position: "fixed",
+                        right: "0",
+                        top: "75"
+                    }}
+                >
+                    <div style={{marginBottom: 20, paddingTop: 50, zIndex: 100}}>
+                        Party Leans for Ohio AAPIs
                     </div>
-                    }
+                    {this.state.selected &&
+                    <>
+                        <div style={{
+                            width: "100%",
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-around"
+                        }}>
+                            <Dropdown options={options}
+                                      controlClassName="dropdown"
+                                      menuClassName="dropdown-menu"
+                                      onChange={(d) => changeElements(d.label)} value={defaultOption}
+                                      placeholder="Select an option"/>
+                        </div>
+                        <ResponsiveCirclePacking
+                            data={getSelected()}
+                            margin={{top: 0, right: 0, bottom: 100, left: 0}}
+                            id="_id"
+                            value="value"
+                            colors={(d) => {
+                                return d.data.color
+                            }}
+                            childColor={{from: 'color', modifiers: [['brighter', 0.4]]}}
+                            colorBy={d => d.color}
+                            padding={4}
+                            enableLabels={true}
+                            labelsFilter={function (e) {
+                                return 2 === e.node.depth
+                            }}
+                            labelsSkipRadius={this.state.labelSkip}
+                            labelTextColor={{from: 'color', modifiers: [['darker', 2]]}}
+                            borderWidth={1}
+                            borderColor={{from: 'color', modifiers: [['darker', 0.5]]}}
+                            tooltip={(input) => (
+                                <>
+                                    {input.data.value &&
+                                    <div id="container"
+                                         style={{backgroundColor: '#ccccccaa', padding: "0.25em"}}>
+                                        <p style={{lineHeight: 0}}>{input.data.type}{input.data._id}</p>
+                                        <p style={{lineHeight: 0}}>{input.data.skew}</p>
+                                        <p style={{lineHeight: 0}}>Population: {input.data.population}</p>
+                                    </div>
+                                    }
 
-                    {!input.data.value &&
-                    <div id="container" style={{backgroundColor: 'rgba(256, 256, 256, 0.4)', padding:"0.25em"}}>
-                      <p style={{lineHeight:0}}>{input.data._id}</p>
-                    </div>}
-                  </>
-                )}
-                defs={[
-                    {
-                        id: 'lines',
-                        type: 'patternLines',
-                        background: 'none',
-                        color: "hsl(192, 70%, 50%)",
-                        rotation: -45,
-                        lineWidth: 0,
-                        spacing: 0,
-                        opacity: 0
+                                    {!input.data.value &&
+                                    <div id="container"
+                                         style={{backgroundColor: '#ccccccaa', padding: "0.25em"}}>
+                                        <p style={{lineHeight: 0}}>{input.data._id}</p>
+                                    </div>}
+                                </>
+                            )}
+                            defs={[
+                                {
+                                    id: 'lines',
+                                    type: 'patternLines',
+                                    background: 'none',
+                                    color: "hsl(192, 70%, 50%)",
+                                    rotation: -45,
+                                    lineWidth: 0,
+                                    spacing: 0,
+                                    opacity: 0
+                                }
+                            ]}
+                            fill={[{match: {depth: 0}, id: 'lines'}]}
+                        />
+                    </>
                     }
-                ]}
-                fill={[ { match: { depth: 0 }, id: 'lines' } ]}
-              />}
-              <div style={{width: "50%", marginTop: -85, marginLeft: "25%"}}>
-              <Dropdown options={options} onChange={(d) => changeElements(d.label)} value={defaultOption} placeholder="Select an option" />
-              </div>
-            </div>
-          </>
+                </div>
+            </>
         );
     }
 
--- src/components/BipartisanEngagement.js
+++ src/components/BipartisanEngagement.js
@@ -1,37 +1,41 @@
 import React, {Component} from "react";
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-import { useState, useEffect } from 'react';
-import { ResponsiveScatterPlot } from '@nivo/scatterplot';
+import {useState, useEffect} from 'react';
+import {ResponsiveScatterPlot} from '@nivo/scatterplot';
 import * as d3 from "d3";
-import {firebaseDatabase,  getBipartisanEngagement} from '../utils/data'
+import {firebaseDatabase, getBipartisanEngagement} from '../utils/data'
 
 import "../App.css";
-import { callbackify } from "util";
+import {callbackify} from "util";
 
 const areaOptions = [
-  "Ohio House Districts", "Ohio Senate Districts", "US House Districts", "Cities",
+    "Ohio House Districts", "Ohio Senate Districts", "US House Districts", "Cities",
 ];
 const defaultAreaOption = areaOptions[0];
 
 const scoreOptions = [
-  "All Elections", "Primary Elections", "General Elections", "Special Elections",
+    "All Elections", "Primary Elections", "General Elections", "Special Elections",
 ];
 const defaultScoreOption = scoreOptions[0];
 
 export default class BipartisanEngagement extends Component {
     state = {
-        stateHouseStats : null,
+        stateHouseStats: null,
         stateSenateStats: null,
         congressionalStats: null,
         cityStats: null,
         selectedArea: null,
-        selectedScore : null,
+        selectedScore: null,
 
     }
 
     componentDidMount() {
-        firebaseDatabase.ref(`summaryStats/stateHouseDistricts`).once('value').then(snapshot => snapshot.val()).then(data => this.setState({stateHouseStats: getBipartisanEngagement(data, "Ohio House District: "), selectedArea: "stateHouseStats", selectedScore: "Score"}))
+        firebaseDatabase.ref(`summaryStats/stateHouseDistricts`).once('value').then(snapshot => snapshot.val()).then(data => this.setState({
+            stateHouseStats: getBipartisanEngagement(data, "Ohio House District: "),
+            selectedArea: "stateHouseStats",
+            selectedScore: "Score"
+        }))
         firebaseDatabase.ref(`summaryStats/stateSenateDistricts`).once('value').then(snapshot => snapshot.val()).then(stats => this.setState({stateSenateStats: getBipartisanEngagement(stats, "Ohio Senate District: ")}));
         firebaseDatabase.ref(`summaryStats/congressionalDistricts`).once('value').then(snapshot => snapshot.val()).then(stats => this.setState({congressionalStats: getBipartisanEngagement(stats, "US House District: ")}));
         firebaseDatabase.ref(`summaryStats/cities`).once('value').then(snapshot => snapshot.val()).then(stats => this.setState({cityStats: getBipartisanEngagement(stats, "City: ")}));
@@ -39,111 +43,150 @@ export default class BipartisanEngagement extends Component {
 
 
     render() {
-      const changeAreaElements = (val) => {
-        if (val === "Cities") {
-          this.setState({selectedArea : "cityStats"});
-        } else if (val === "Ohio House Districts") {
-          this.setState({selectedArea : "stateHouseStats"});
-        } else if (val === "Ohio Senate Districts") {
-          this.setState({selectedArea : "stateSenateStats"});
-        } else {
-          this.setState({selectedArea : "congressionalStats"});
-        }
-      };
+        const changeAreaElements = (val) => {
+            if (val === "Cities") {
+                this.setState({selectedArea: "cityStats"});
+            } else if (val === "Ohio House Districts") {
+                this.setState({selectedArea: "stateHouseStats"});
+            } else if (val === "Ohio Senate Districts") {
+                this.setState({selectedArea: "stateSenateStats"});
+            } else {
+                this.setState({selectedArea: "congressionalStats"});
+            }
+        };
 
-      const changeScoreElements = (val) => {
-        if (val === "All Elections") {
-          this.setState({selectedScore : "Score"});
-        } else if (val === "Primary Elections") {
-          this.setState({selectedScore : "Primary"});
-        } else if (val === "General Elections") {
-          this.setState({selectedScore : "General"});
-        } else {
-          this.setState({selectedScore : "Special"});
-        }
-      };
+        const changeScoreElements = (val) => {
+            if (val === "All Elections") {
+                this.setState({selectedScore: "Score"});
+            } else if (val === "Primary Elections") {
+                this.setState({selectedScore: "Primary"});
+            } else if (val === "General Elections") {
+                this.setState({selectedScore: "General"});
+            } else {
+                this.setState({selectedScore: "Special"});
+            }
+        };
 
-      const getSelected = () => {
-        if (this.state.selectedArea === "cityStats") {
-          return this.state.cityStats[this.state.selectedScore];
-        } else if (this.state.selectedArea === "stateHouseStats") {
-          return this.state.stateHouseStats[this.state.selectedScore];
-        } else if (this.state.selectedArea === "stateSenateStats") {
-          return this.state.stateSenateStats[this.state.selectedScore];
-        } else {
-          return this.state.congressionalStats[this.state.selectedScore];
-        }
-      };
-      if (this.state.cityStats) {
+        const getSelected = () => {
+            if (this.state.selectedArea === "cityStats") {
+                return this.state.cityStats[this.state.selectedScore];
+            } else if (this.state.selectedArea === "stateHouseStats") {
+                return this.state.stateHouseStats[this.state.selectedScore];
+            } else if (this.state.selectedArea === "stateSenateStats") {
+                return this.state.stateSenateStats[this.state.selectedScore];
+            } else {
+                return this.state.congressionalStats[this.state.selectedScore];
+            }
+        };
+        if (this.state.cityStats) {
             console.log(this.state.cityStats["Special"]);
-      }
-      return (
-        <div
-            style={{width:'55%', paddingRight:'5%', paddingBottom:"75px", paddingLeft:"5%", height: "calc(100vh - 150px)", position:"fixed", right:"0", top:"75"}}
-        >
-        <div style={{marginBottom:-45, paddingTop:0, zIndex:100}}> Bipartisan Relationships with Ohio AAPI Voter Turnout</div>
-        {this.state.selectedArea && this.state.selectedScore && <ResponsiveScatterPlot
-            animate={(this.state.selectedArea === 'cityStats') ? false : true}
-            data={getSelected()}
-            colors = {(d) => d.color}
-            style={{color:"white"}}
-            nodeSize = {12}
-            margin={{ top: 100, right: 100, bottom: 150, left: 100 }}
-            xScale={{ type: 'linear', min: 'auto', max: 'auto' }}
-            xFormat={function(e){return e}}
-            yScale={{ type: 'linear', min: 'auto', max: 'auto' }}
-            yFormat={function(e){return e}}
-            blendMode="multiply"
-            axisTop={null}
-            axisFormat={{color:"white"}}
-            tooltip={({ node }) => (
+        }
+        return (
             <div
                 style={{
-                    color: node.style.color,
-                    background: '#333',
-                    padding: '12px 16px',
+                    width: '55%',
+                    paddingRight: '5%',
+                    paddingBottom: "75px",
+                    paddingLeft: "5%",
+                    height: "calc(100vh - 150px)",
+                    position: "fixed",
+                    right: 0,
+                    top: 75,
+                    display: "flex",
+                    flexDirection: "column"
                 }}
             >
-                <strong>
-                    {node.data.type} {node.data._id}
-                </strong>
-                <br />
-                {`Bipartisan Skew: ${node.data.x}`}
-                <br />
-                {`Engagement: ${node.data.y.toFixed(3)}`}
+                <div style={{marginBottom: 20, paddingTop: 50, zIndex: 100}}>
+                    Party Relationships with Ohio AAPI Voter Turnout
                 </div>
-            )}
-            axisRight={{
-                orient: 'left',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-            }}
+                {this.state.selectedArea && this.state.selectedScore &&
+                <>
+                    <div style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-around"
+                    }}>
+                        <Dropdown style={{width: 200}}
+                                  options={areaOptions}
+                                  controlClassName="dropdown"
+                                  menuClassName="dropdown-menu"
+                                  onChange={(d) => changeAreaElements(d.label)}
+                                  value={defaultAreaOption} placeholder="Select an option"/>
+                        <Dropdown style={{width: 200}}
+                                  options={scoreOptions}
+                                  controlClassName="dropdown"
+                                  menuClassName="dropdown-menu"
+                                  onChange={(d) => changeScoreElements(d.label)}
+                                  value={defaultScoreOption} placeholder="Select an option"/>
+                    </div>
+                    <ResponsiveScatterPlot
+                        animate={(this.state.selectedArea === 'cityStats') ? false : true}
+                        data={getSelected()}
+                        colors={(d) => d.color}
+                        style={{color: "white"}}
+                        nodeSize={12}
+                        margin={{top: 50, right: 80, bottom: 100, left: 80}}
+                        xScale={{type: 'linear', min: 'auto', max: 'auto'}}
+                        xFormat={function (e) {
+                            return e
+                        }}
+                        yScale={{type: 'linear', min: 'auto', max: 'auto'}}
+                        yFormat={function (e) {
+                            return e
+                        }}
+                        blendMode="multiply"
+                        axisTop={null}
+                        axisFormat={{color: "white"}}
+                        tooltip={({node}) => (
+                            <div
+                                style={{
+                                    color: node.style.color,
+                                    background: '#333',
+                                    padding: '12px 16px',
+                                }}
+                            >
+                                <strong>
+                                    {node.data.type} {node.data._id}
+                                </strong>
+                                <br/>
+                                {`Bipartisan Skew: ${node.data.x}`}
+                                <br/>
+                                {`Engagement: ${node.data.y.toFixed(3)}`}
+                            </div>
+                        )}
+                        axisRight={{
+                            orient: 'left',
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                        }}
 
-            axisBottom={{
-                orient: 'bottom',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'Bipartisan Skew',
-                legendPosition: 'middle',
-                legendOffset: 46
-            }}
-            axisLeft={{
-                orient: 'left',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'Average Voter Engagement Score',
-                legendPosition: 'middle',
-                legendOffset: -60
-            }}
-          />}
-          <div style={{width: "80%", marginTop: -85, marginLeft: "25%", display:"flex", flexDirection:"horizontal"}}>
-          <Dropdown style={{width:200}} options={areaOptions} onChange={(d) => changeAreaElements(d.label)} value={defaultAreaOption} placeholder="Select an option" />
-          <Dropdown style={{width:200}} options={scoreOptions} onChange={(d) => changeScoreElements(d.label)} value={defaultScoreOption} placeholder="Select an option" />
-          </div>
-          </div>
-    )};
+                        axisBottom={{
+                            orient: 'bottom',
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                            legend: 'Bipartisan Skew',
+                            legendPosition: 'middle',
+                            legendOffset: 46
+                        }}
+                        axisLeft={{
+                            orient: 'left',
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                            legend: 'Average Voter Engagement Score',
+                            legendPosition: 'middle',
+                            legendOffset: -60
+                        }}
+                    />
+                </>
+                }
+            </div>
+        )
+    }
+
+    ;
 
 }
